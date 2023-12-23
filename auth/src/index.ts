245b837e95315bc2176ec746c71c0cc646a66dc9
Chris000600 <christopherwilliam00@yahoo.com>
2023-12-23 12:10:51 +0700

    setting up tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,5 @@
-import express from 'express';
-import 'express-async-errors'; // use this to handle async errors
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true); // trust ingress
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-);
-
-// routing
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-// error handling
-// unknown route error handling
-app.all('*', async (req, res, next) => {
-  throw new NotFoundError();
-});
-
-// use the error handling middleware created
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   // ensure env variables were set
